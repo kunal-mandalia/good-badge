6aa0ee949aee19c7f8c3d00439130896087a1d66
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-12-26 22:43:11 +0000

    feat: basic theming

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
-import { makeStyles, Typography } from '@material-ui/core'
+import {
+  createMuiTheme,
+  makeStyles,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     textAlign: 'center',
     padding: theme.spacing(4),
+    color: theme.palette.text.primary,
+    minHeight: `calc(100vh - ${theme.spacing(8)}px)`,
+    borderRadius: 0,
   },
 }))
+
+export const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
 
 function App() {
   const classes = useStyles()
   return (
-    <div className={classes.root}>
-      <Typography variant="h2">Good Badge</Typography>
-      <Typography variant="subtitle1">
-        Embed links for charities you support
-      </Typography>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Paper className={classes.root}>
+        <Typography variant="h2" color="textPrimary">
+          Good Badge
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          Embed links for charities you support
+        </Typography>
+      </Paper>
+    </ThemeProvider>
   )
 }
 
